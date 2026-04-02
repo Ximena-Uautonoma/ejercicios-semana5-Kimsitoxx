5d22262e1c495ba7fd89910c9b54a140886ce354
Kimsitoxx <cristopher.martinez.reb@gmail.com>
2026-04-02 19:45:48 +0000

    ejercicios completos

--- Semana5/Ejercicio4.py
+++ Semana5/Ejercicio4.py
@@ -5,5 +5,11 @@ Solicita repetidamente:
 0 para terminar
 Al final, muestra cuántos días asistió.'''
 
+si = 1
+asistencia = 0
+while si == 1:
+    si = int(input("Confirme asistencia del dia de hoy (1 Si, 0 No): "))
+    if si == 1:
+        asistencia = asistencia + 1
 
-
+print("Asistió", asistencia, "dias.")
